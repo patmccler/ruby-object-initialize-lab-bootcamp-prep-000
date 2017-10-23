63b6efcb54112327c7a649e7d8f322e02de5058e
Patrick McClernan <patmccler@gmail.com>
2017-10-23 23:18:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 
 class Dog
 
-  def initialize(name,breed="Muttlearn")
+  def initialize(name,breed="Mutt")
     @name = name
     @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def name=(name)
+    @name = name
   end
 
   def breed
